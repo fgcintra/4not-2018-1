d59798cd595d5dc289cb5e188d03e1d10aae28a2
fgcintra <fausto@fgcintra.pro.br>
2018-03-24 08:42:43 -0300

    (24/03) Finalização do controller para modelo do Mongoose

--- back-end/controllers/produto.js
+++ back-end/controllers/produto.js
@@ -21,7 +21,7 @@ module.exports = function() {
 
    controller.obterUm = function(req, res) {
       var idProduto = req.params.id;
-      Produto.find({_id: idProduto}).exec().then(
+      Produto.findById(idProduto).exec().then(
          function(produto) {
             // Produto não encontrado (a variável vem vazia)
             if(! produto) {
@@ -53,6 +53,46 @@ module.exports = function() {
          }
       });
 
+   }
+
+   controller.alterar = function(req, res) {
+      var idProduto = req.params.id;
+
+      if(idProduto) { // Foi psassado o id do produto
+         Produto.findByIdAndUpdate(idProduto, req.body).exec().then(
+            function(contato) {
+               // HTTP 204: No content
+               res.status(204).end();
+            },
+            function(erro) {
+               console.error(erro);
+               res.status(500).json(erro).end();
+            }
+         );
+      }
+      else {
+         // HTTP 400: Bad request
+         res.status(400).end();
+      }
+   }
+
+   controller.excluir = function(req, res) {
+      var idProduto = req.params.id;
+
+      if(idProduto) {
+         Produto.findByIdAndRemove(idProduto).exec().then(
+            function(produto) {
+               res.status(204).end();
+            },
+            function(erro) {
+               console.error(erro);
+               res.status(500).json(erro).end();
+            }
+         );
+      }
+      else {
+         res.status(400).end();
+      }
    }
 
    return controller;
--- back-end/routes/produto.js
+++ back-end/routes/produto.js
@@ -8,8 +8,8 @@ router.get('/:id', controller().obterUm);
 
 router.put('/', controller().novo);
 
-//router.post('/:id', controller().alterar);
+router.post('/:id', controller().alterar);
 
-//router.delete('/:id', controller().excluir);
+router.delete('/:id', controller().excluir);
 
 module.exports = router;
